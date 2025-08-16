6d88787dc3728c84c6790e376a5c05155efa005a
LauraLopez0110 <128760270+LauraLopez0110@users.noreply.github.com>
2025-08-15 23:47:49 -0500

    Se creo los diferentes archivos para el proyecto, esta totalmente sin estilos solo algo funcional

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, abort
+import json
+
+app = Flask(__name__)
+
+# Cargar los datos desde el JSON
+with open("data/integrantes.json", encoding="utf-8") as f:
+    integrantes = json.load(f)
+
+@app.route("/")
+def index():
+    # Pasamos todos los integrantes al index
+    return render_template("index.html", integrantes=integrantes)
+
+@app.route("/perfil/<slug>")
+def perfil(slug):
+    if slug not in integrantes:
+        abort(404)
+    return render_template("perfil.html", perfil=integrantes[slug])
+
+if __name__ == "__main__":
+    app.run(debug=True)
